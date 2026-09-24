f968fc678926a9694be5a949475ff6a8f605f5ce
kartik <kalyanshettikartik60@gmail.com>
2026-09-24 13:09:17 +0530

    Bash Options Arguments

---
+++ Chapter_5_to_7/Bash_Args/Simple_script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+First_name="Kartik"
+Last_name="Kalyanshetti"
+
+full_name="$First_name $Last_name"
+
+if [[ $First_name == 'Nakul' ]]; then
+	echo "Hey! It's Nakul"
+fi
+
+echo "Hello $full_name!"
+
---
+++ Chapter_5_to_7/Bash_Args/undefined.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+foo=1
+bar=2
+
+echo "foo is $foo"
+echo "bar is $bar"
+echo "baz is $baz"
